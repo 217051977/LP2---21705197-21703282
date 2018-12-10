3516a146ed71bf0131a9345939c0f2b54ab79242
21705197 <a21705197@alunos.ulht.pt>
2018-12-10 23:40:36 +0000

    Atualização

--- src/pt/ulusofona/lp2/crazyChess/Joker.java
+++ src/pt/ulusofona/lp2/crazyChess/Joker.java
@@ -3,37 +3,26 @@ package pt.ulusofona.lp2.crazyChess;
 public class Joker extends CrazyPiece{
 
     private byte id = 7;
+    private byte pieceId = 1;
 
     private boolean moveCima, moveBaixo, moveEsquerda, moveDireita,
             moveCimaDireita, moveCimaEsquerda, moveBaixoDireita, moveBaixoEsquerda;
 
     private int maxMovHorizontal, maxMovVertical, minMovHorizontal, minMovVertical;
 
-    Joker() {
+    Joker() {}
 
-//        this.moveBaixo = true;
-//        this.moveEsquerda = true;
-//        this.moveDireita = true;
-//        this.moveCima = true;
-//        this.moveCimaDireita = true;
-//        this.moveCimaEsquerda = true;
-//        this.moveBaixoDireita = true;
-//        this.moveBaixoEsquerda = true;
-//        this.maxMovHorizontal = 1;
-//        this.maxMovVertical = 1;
-//        this.minMovHorizontal = 0;
-//        this.minMovVertical = 0;
+    public void changePieceType() {
 
-//        gets the mov of another piece
-//        Turno 0 - Rainha
-//        Turno 1 - Pónei Mágico
-//        Turno 2 - Padre da Vila
-//        Turno 3 - Torre Hor
-//        Turno 4 - Torre Vert
-//        Turno 5 - Lebre
-//        Turno 6 - Rainha (novamente)
-//        Turno 7 - Pónei Mágico (novamente)
-//        etc.
+        if (pieceId == 7) {
+
+            pieceId = 1;
+
+        } else {
+
+            pieceId++;
+
+        }
 
     }
 
